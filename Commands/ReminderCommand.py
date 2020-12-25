d6327d9878bd21002b8552523c513c3516c14a92
sciff16 <sciff16@yandex.ru>
2020-12-25 21:31:14 +0700

    Fixed bugs

--- Commands/ReminderCommand.py
+++ Commands/ReminderCommand.py
@@ -6,13 +6,25 @@ from time import sleep
 from Commands.AbstractCommand import AbstractCommand
 
 
+class Reminder(object):
+    def __init__(self, reminder_text: str, reminder_time: time):
+        self.text = reminder_text
+        self.time = reminder_time
+
+    @property
+    def reminder_text(self):
+        return self.text
+
+    @property
+    def reminder_time(self):
+        return self.time
+
+
 class ReminderCommand(AbstractCommand):
     def __init__(self):
-        self.reminder_text = None
-        self.reminder_time = datetime.now().time()
+        self.reminders = list()
         self.notify_title = "Напоминалка"
         self.notify_app_name = "VirtualAssistant"
-        self.enable_reminder = False
         reminder_task = Thread(target=self.send_remind, daemon=True)
         reminder_task.start()
 
@@ -28,36 +40,34 @@ class ReminderCommand(AbstractCommand):
         return command == self.name
 
     def enter_reminder_args(self) -> bool:
-        self.reminder_text = str(input('О чём вам напомнить?\n'))
+        text = str(input('О чём вам напомнить?\n'))
         time_entry = input('Введите время напоминания (в формате часы:минуты):\n')
         try:
             hour, minutes = map(int, time_entry.split(':'))
-            self.reminder_time = time(hour, minutes)
-            if self.reminder_time < datetime.now().time():
+            _time = time(hour, minutes)
+            if _time < datetime.now().time():
                 raise ValueError
         except ValueError:
             print("Ошибка при вводе времени\n")
             return False
+        reminder = Reminder(text, _time)
+        self.reminders.append(reminder)
+        self.reminders.sort(key=lambda x: x.time)
         return True
 
     def send_remind(self):
         while True:
-            if self.enable_reminder:
-                if self.reminder_time.hour == datetime.now().time().hour and \
-                        self.reminder_time.minute == datetime.now().time().minute:
+            for reminder in self.reminders:
+                if reminder.reminder_time.hour == datetime.now().time().hour and \
+                        reminder.reminder_time.minute == datetime.now().time().minute:
                     plyer.notification.notify(
-                        message=self.reminder_text,
+                        message=reminder.reminder_text,
                         app_name=self.notify_app_name,
                         title=self.notify_title,
                     )
-                    self.enable_reminder = False
+                    self.reminders.remove(reminder)
             sleep(1)
 
     def execute(self):
         if not self.enter_reminder_args():
-            return
-        if not self.enable_reminder:
-            self.enable_reminder = True
-        else:
-            print('Напоминание уже установлено\n')
             return
